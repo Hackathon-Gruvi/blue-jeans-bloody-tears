21a723b002890960cb33cae4dde09912b9afdc45
TitoGrine <tito999@gmail.com>
2021-04-10 16:13:28 +0100

    Changed return for TMDB api.

--- backend/src/routes/resources/tmdb.js
+++ backend/src/routes/resources/tmdb.js
@@ -3,16 +3,78 @@ const { MovieDb } = require("moviedb-promise");
 
 const moviedb = new MovieDb("8dda66a612926665b37d33db21eca331");
 
+const getMovieDetails = async (id) => {
+  return await moviedb
+    .movieInfo(id)
+    .then((res) => {
+      let genres = [];
+      let companies = [];
+
+      if (res.genres !== undefined) {
+        res.genres.forEach((genre) => {
+          genres.push(genre.name);
+        });
+      }
+
+      if (res.production_companies !== undefined) {
+        res.production_companies.forEach((company) => {
+          companies.push(company.name);
+        });
+      }
+
+      return {
+        imdb_id: res.imdb_id,
+        lenght: res.runtime,
+        genres,
+        companies,
+      };
+    })
+    .catch((err) => {
+      console.log(err);
+      return null;
+    });
+};
+
 router.get("/", async (req, res) => {
   const { q } = req.query;
 
   moviedb
     .searchMovie({ query: q })
-    .then((result) => {
-      console.log(result);
-      res.json(result);
+    .then(async (result) => {
+      let matches = [];
+
+      if (result.results.length === 0) res.json([]);
+
+      for (let i = 0; i < result.results.length; i++) {
+        let movie = result.results[i];
+        let info = await getMovieDetails(movie.id);
+
+        let date;
+        if (movie.release_date !== undefined)
+          date = movie.release_date.length > 0 ? movie.release_date : undefined;
+
+        let output = {
+          title: movie.title,
+          original_title: movie.original_title,
+          date,
+          language: movie.original_language,
+        };
+
+        if (info !== undefined && info !== null) {
+          output.imdb_id = info.imdb_id;
+          output.length = info.length;
+          output.genres = info.genres;
+          output.companies = info.companies;
+        }
+
+        matches.push(output);
+      }
+
+      res.json(matches);
     })
-    .catch(console.error);
+    .catch((error) => {
+      res.status(500).send(error);
+    });
 });
 
 module.exports = router;
