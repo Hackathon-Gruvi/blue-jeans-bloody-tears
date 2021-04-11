3d1aff3063402616baafc74bcf72fe9fbad56492
TitoGrine <tito999@gmail.com>
2021-04-11 15:07:32 +0100

    More exception handling.

--- backend/src/routes/helper/imdb.js
+++ backend/src/routes/helper/imdb.js
@@ -5,48 +5,55 @@ const { runtime_2_min } = require("../../utils/utils.js");
 
 module.exports.getIMDBData = async (query) =>
   new Promise((resolve, reject) => {
-    nameToImdb(query, async function (err, result, information) {
-      if (err || result === undefined) {
-        resolve([]);
-      }
+    const d = require("domain").create();
+    d.on("error", (err) => {
+      resolve([]);
+    });
 
-      imdb(result, async function (err, data) {
-        if (err) {
-          console.log(err.stack);
+    d.run(() => {
+      nameToImdb(query, async function (err, result, information) {
+        if (err || result === undefined) {
           resolve([]);
         }
 
-        if (!data) {
-          resolve([]);
-        }
+        imdb(result, async function (err, data) {
+          if (err) {
+            console.log(err.stack);
+            resolve([]);
+          }
 
-        let output = {
-          imdb_id: result,
-          title: data.title ? data.title.trim() : "",
-          year: data.year,
-          length: runtime_2_min(data.runtime),
-          genres: data.genre,
-          director: data.director !== "N/A" ? data.director : undefined,
-          writer: data.writer !== "N/A" ? data.writer : undefined,
-        };
+          if (!data) {
+            resolve([]);
+          }
 
-        let factor = 0.596;
-        if (
-          output.title
-            .replaceAll(" ", "")
-            .toLowerCase()
-            .includes(query.replaceAll(" ", "").toLowerCase())
-        )
-          factor *= 1.2;
-        if (output.year.length > 0) factor *= 1.1;
-        if (output.length > 0) factor *= 1.1;
-        if (output.genres.length > 0) factor *= 1.1;
-        if (output.writer !== undefined || output.director !== undefined)
-          factor *= 1.05;
+          let output = {
+            imdb_id: result,
+            title: data.title ? data.title.trim() : "",
+            year: data.year,
+            length: runtime_2_min(data.runtime),
+            genres: data.genre,
+            director: data.director !== "N/A" ? data.director : undefined,
+            writer: data.writer !== "N/A" ? data.writer : undefined,
+          };
 
-        output.factor = factor;
+          let factor = 0.596;
+          if (
+            output.title
+              .replaceAll(" ", "")
+              .toLowerCase()
+              .includes(query.replaceAll(" ", "").toLowerCase())
+          )
+            factor *= 1.2;
+          if (output.year.length > 0) factor *= 1.1;
+          if (output.length > 0) factor *= 1.1;
+          if (output.genres.length > 0) factor *= 1.1;
+          if (output.writer !== undefined || output.director !== undefined)
+            factor *= 1.05;
 
-        resolve([output]);
+          output.factor = factor;
+
+          resolve([output]);
+        });
       });
     });
   });
--- backend/src/routes/helper/rapid.js
+++ backend/src/routes/helper/rapid.js
@@ -6,7 +6,9 @@ module.exports.getRapidData = async (query) =>
   new Promise((resolve, reject) => {
     const options = {
       method: "GET",
-      url: `https://imdb-internet-movie-database-unofficial.p.rapidapi.com/search/${query}`,
+      url: `https://imdb-internet-movie-database-unofficial.p.rapidapi.com/search/${encodeURI(
+        query
+      )}`,
       headers: {
         Accept: "*/*",
         "Accept-Encoding": "gzip, deflate",
@@ -17,54 +19,66 @@ module.exports.getRapidData = async (query) =>
       },
     };
 
-    axios
-      .request(options)
-      .then(function (response) {
-        const titles = response.data.titles;
-        if (!titles) resolve([]);
+    const d = require("domain").create();
+    d.on("error", (err) => {
+      resolve([]);
+    });
 
-        const titleRequests = titles.map((data) => {
-          const url = `https://imdb-internet-movie-database-unofficial.p.rapidapi.com/film/${data.id}`;
-          return axios.get(url, options);
-        });
+    d.run(() => {
+      axios
+        .request(options)
+        .then(function (response) {
+          const titles = response.data.titles;
+          if (!titles) resolve([]);
 
-        return Promise.all(titleRequests).then((result) => {
-          const output = result.map((res) => {
-            const film = {
-              title: res.data.title ? res.data.title.trim() : "",
-              year: res.data.year,
-              imdb_id: res.data.id,
-              length: runtime_2_min(res.data.length),
-              rating_votes: res.data.rating_votes,
-              cast:
-                res.data.cast === undefined
-                  ? []
-                  : res.data.cast.map((actor) => actor.actor),
-            };
-
-            let factor = 0.596;
-            if (
-              film.title
-                .replaceAll(" ", "")
-                .toLowerCase()
-                .includes(query.replaceAll(" ", "").toLowerCase())
-            )
-              factor *= 1.2;
-            if (film.year.length > 0) factor *= 1.1;
-            if (film.length > 0) factor *= 1.1;
-            if (film.rating_votes.length > 0) factor *= 1.1;
-            if (film.cast.length > 0) factor *= 1.05;
-
-            film.factor = factor;
-
-            return film;
+          const titleRequests = titles.map((data) => {
+            const url = `https://imdb-internet-movie-database-unofficial.p.rapidapi.com/film/${data.id}`;
+            return axios.get(url, options);
           });
 
-          resolve(output);
+          return Promise.all(titleRequests)
+            .then((result) => {
+              const output = result.map((res) => {
+                const film = {
+                  title: res.data.title ? res.data.title.trim() : "",
+                  year: res.data.year,
+                  imdb_id: res.data.id,
+                  length: runtime_2_min(res.data.length),
+                  rating_votes: res.data.rating_votes,
+                  cast:
+                    res.data.cast === undefined
+                      ? []
+                      : res.data.cast.map((actor) => actor.actor),
+                };
+
+                let factor = 0.596;
+                if (
+                  film.title
+                    .replaceAll(" ", "")
+                    .toLowerCase()
+                    .includes(query.replaceAll(" ", "").toLowerCase())
+                )
+                  factor *= 1.2;
+                if (film.year.length > 0) factor *= 1.1;
+                if (film.length > 0) factor *= 1.1;
+                if (film.rating_votes.length > 0) factor *= 1.1;
+                if (film.cast.length > 0) factor *= 1.05;
+
+                film.factor = factor;
+
+                return film;
+              });
+
+              resolve(output);
+            })
+            .catch((err) => {
+              console.error(error);
+              resolve([]);
+            });
+        })
+        .catch(function (error) {
+          console.error(error);
+          resolve([]);
         });
-      })
-      .catch(function (error) {
-        console.error(error);
-        resolve([]);
-      });
+    });
   });
